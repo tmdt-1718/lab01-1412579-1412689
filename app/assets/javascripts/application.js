fedf91cc10da096bb217926d8ca387bcf784fceb
Vũ Minh Trí <1412579@student.hcmus.edu.vn>
2017-10-09 10:13:13 +0700

    update precompile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //= require jquery
 //= require jquery_ujs
-//= require rails-ujs
+
 //= require jquery.turbolinks
+//= require rails-ujs
 //= require popper.min
 
 //= require turbolinks
