167c1d16d2bec2b22f04acca045467a1909a7f5d
Vũ Minh Trí <1412579@student.hcmus.edu.vn>
2017-10-09 09:24:43 +0700

    change appl.js to test_fix name

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //= require jquery3
-//= require popper
+//= require popper.min
 //= require bootstrap-sprockets
 //= require rails-ujs
 //= require turbolinks
