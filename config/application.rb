86350054784bf77c709bb5dae92137081768ca09
Vũ Minh Trí <1412579@student.hcmus.edu.vn>
2017-10-09 08:48:21 +0700

    adding config

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ module Lab0114125791412689
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,10 +69,10 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
-  config.serve_static_assets = true
-  config.assets.compile = true
+  config.assets.compile = ['*.js', '*.css']
+  config.active_support.deprecation = :silence
   # Send deprecation notices to registered listeners.
-  config.active_support.deprecation = :notify
+  #config.active_support.deprecation = :notify
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
