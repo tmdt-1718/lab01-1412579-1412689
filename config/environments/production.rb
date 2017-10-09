6e62f65aa5c999190372f51f32335cca28e9754e
Vũ Minh Trí <1412579@student.hcmus.edu.vn>
2017-10-09 10:18:12 +0700

    update precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,8 +72,11 @@ Rails.application.configure do
   
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
-  config.serve_static_assets = true
+  config.serve_static_assets = false
   config.assets.compress = true
+  config.assets.js_compressor = :uglifier
+  config.assets.digest = true
+  config.assets.compile = true  
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
